b41ee3a3f626b315b45baefddd58c2a9b92586aa
Oladeji <oladeji.okuneye@gmail.com>
2025-05-19 07:41:09 +0100

    modified index.js in the frontend

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('E-commerce Frontend is running');
+  res.end('E-commerce Frontend is currently running');
 });
 
 server.listen(3000, () => {
